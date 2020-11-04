f7f92b58e1f78e710832fd098538e505b4db9696
MaksimLialkin <maksimlialkin1986@gmail.com>
2020-11-04 11:41:12 +0300

    NavLink, activeLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
+import { NavLink } from 'react-router-dom'
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div><a href="/main">Обо мне</a></div>
-            <div><a href="/dialogs">Мой опыт</a></div>
-            <div><a href="#">Мои навыки</a></div>
-            <div><a href="#">Мои работы</a></div>
-            <div><a href="#">Прайс-лист</a></div>
+            <div className={classes.item}><NavLink to="/main" activeClassName={classes.active}>Обо мне</NavLink></div>
+            <div className={classes.item}><NavLink to="/dialogs" activeClassName={classes.active}>Мой опыт</NavLink></div>
+            <div className={classes.item}><a href="#">Мои навыки</a></div>
+            <div className={classes.item}><a href="#">Мои работы</a></div>
+            <div className={classes.item}><a href="#">Прайс-лист</a></div>
         </nav>
     )
 }
