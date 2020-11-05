e860d8570652ac2812574bda673181cdb37af842
MaksimLialkin <maksimlialkin1986@gmail.com>
2020-11-06 00:46:27 +0300

    Navlink,route,exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,13 +6,13 @@ const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={classes.dialog + " " + classes.active}>Максим</div>
-                <div className={classes.dialog}>Мария</div>
-                <div className={classes.dialog}>Игорь</div>
-                <div className={classes.dialog}>Сергей</div>
-                <div className={classes.dialog}>Борис</div>
-                <div className={classes.dialog}>Валерий</div>
-                <div className={classes.dialog}>Ирина</div>
+                <div className={classes.dialog}><NavLink to="/dialogs/1" activeClassName={classes.active}>Максим</NavLink></div>
+                <div className={classes.dialog}><NavLink to="/dialogs/2" activeClassName={classes.active}>Мария</NavLink></div>
+                <div className={classes.dialog}><NavLink to="/dialogs/3" activeClassName={classes.active}>Игорь</NavLink></div>
+                <div className={classes.dialog}><NavLink to="/dialogs/4" activeClassName={classes.active}>Сергей</NavLink></div>
+                <div className={classes.dialog}><NavLink to="/dialogs/5" activeClassName={classes.active}>Борис</NavLink></div>
+                <div className={classes.dialog}><NavLink to="/dialogs/6" activeClassName={classes.active}>Валерий</NavLink></div>
+                <div className={classes.dialog}><NavLink to="/dialogs/7" activeClassName={classes.active}>Ирина</NavLink></div>
             </div>
             <div className={classes.messages}>
                 <div className={classes.message}>Привет, как дела?</div>
