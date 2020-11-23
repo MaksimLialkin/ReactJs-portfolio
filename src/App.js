905e0449b5de323e93d8c4c9959054a9b2e5367d
MaksimLialkin <maksimlialkin1986@gmail.com>
2020-11-23 23:07:19 +0300

    upgrade link Main

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main';
 
-
 const App = (props) => {
   return (
     <BrowserRouter>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,26 @@
+import classes from './Main.module.css'
+
 const Main = () => {
     return (
-        <div></div>
+        <div className={classes.about}>
+            <div className={classes.aboutWrapper}>
+                <div className={classes.aboutImg}></div>
+                <div className={classes.aboutDescr}>
+                    <div className={classes.Descr_title}>Про меня</div>
+                    <div className={classes.Descr_subtitle}>Меня зовут Максим</div>
+                    <div className={classes.divider}></div>
+                    <p className={classes.Descr_text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla nostrum placeat cupiditate vitae similique rerum recusandae dolor sunt natus provident ratione impedit rem, odio unde tempora doloremque tempore non ducimus!</p>
+                </div>
+                <div className={classes.aboutSkills}>
+                    <div className={classes.Skills_item}>
+                    </div>
+                    <div className={classes.Skills_item}>
+                    </div>
+                    <div className={classes.Skills_item}>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
-
 export default Main;
