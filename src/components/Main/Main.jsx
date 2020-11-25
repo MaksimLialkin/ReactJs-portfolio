faf6e7f150045c4ed85b95bc67221f19ee91b5fe
MaksimLialkin <maksimlialkin1986@gmail.com>
2020-11-25 16:52:04 +0300

    adding new component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import classes from './Main.module.css'
+import MainInfo from './MainInfo/MainInfo';
 import MainSkills from './MainSkills/MainSkills'
 
 const Main = () => {
@@ -6,12 +7,7 @@ const Main = () => {
         <div className={classes.main}>
             <div className={classes.main_wrapper}>
                 <div className={classes.main_photo}></div>
-                <div className={classes.main_descr}>
-                    <h2 className={`${classes.title} ${classes.title_fz20} ${classes.subtitle}`}>Обо мне</h2>
-                    <div className={classes.main_name}>Меня зовут Максим</div>
-                    <div className={classes.devider}></div>
-                    <p className={classes.main_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium repellat tenetur libero veritatis dolores architecto reprehenderit unde blanditiis laborum, nostrum, labore earum. Architecto molestias quos fugit nemo quo sapiente temporibus.</p>
-                </div>
+                <MainInfo />
                 <MainSkills />
             </div>
         </div>
---
+++ src/components/Main/MainInfo/MainInfo.jsx
@@ -0,0 +1,16 @@
+import classes from './MainInfo.module.css'
+
+const MainInfo = () => {
+    return (
+        <div>
+            <div className={classes.main_descr}>
+                <h2 className={`${classes.title} ${classes.title_fz20} ${classes.subtitle}`}>Обо мне</h2>
+                <div className={classes.main_name}>Меня зовут Максим</div>
+                <div className={classes.devider}></div>
+                <p className={classes.main_text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium repellat tenetur libero veritatis dolores architecto reprehenderit unde blanditiis laborum, nostrum, labore earum. Architecto molestias quos fugit nemo quo sapiente temporibus.</p>
+            </div>
+        </div>
+    )
+}
+
+export default MainInfo;
